def7ff68b9cecfab68f020e2df1690239e1d8304
duddns6290 <duddns6290@naver.com>
2025-09-02 10:10:41 +0900

    init: base 프로젝트 업로드

---
+++ src/main/java/com/goorm/derere/DerereApplication.java
@@ -0,0 +1,13 @@
+package com.goorm.derere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DerereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DerereApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goorm/derere/DerereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goorm.derere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DerereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
