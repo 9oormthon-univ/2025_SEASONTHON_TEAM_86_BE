b94031870f2703041237bfcf2d98d0b0d2f61a16
류재민 <ryumin0421@gmail.com>
2025-09-02 18:07:19 +0900

    init: SpringSecurity 세팅

---
+++ src/main/java/com/goorm/derere/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.goorm.derere;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf((csrf) -> csrf.disable());
+
+        http.authorizeHttpRequests((authorize) ->
+                authorize.requestMatchers("/**").permitAll() // 개발 단계에서 임시로 모든 URL 허용
+        );
+
+        return http.build();
+    }
+}
