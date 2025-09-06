11e19d5f438d4d9d26b4da8b78f3f53d051830db
duddns6290 <duddns6290@naver.com>
2025-09-06 23:57:14 +0900

    feat: 카카오 로그인 세션 저장 수정

--- src/main/java/com/goorm/derere/controller/SignupController.java
+++ src/main/java/com/goorm/derere/controller/SignupController.java
@@ -23,34 +23,18 @@ public class SignupController {
     private final OAuthRepository oAuthRepository;
     private final LoginRedirectService loginRedirectService;
 
-//    @PostMapping("/signup")
-//    public User signup(@AuthenticationPrincipal OAuthUserInfo oauthUser,
-//                       @RequestBody Map<String, String> body) {
-//
-//        if (oauthUser == null) {
-//            throw new IllegalStateException("OAuth 정보가 없습니다.");
-//        }
-//
-//        User newUser = User.builder()
-//                .username(oauthUser.getUsername())      // OAuth 닉네임
-//                .email(oauthUser.getEmail())            // OAuth 이메일
-//                .location(body.get("location"))         // React에서 보낸 JSON
-//                .usertype(body.get("usertype"))         // React에서 보낸 JSON
-//                .build();
-//
-//        newUser = oAuthRepository.save(newUser);
-//
-////        OAuthLoginResult result = new OAuthLoginResult(newUser, false); // 신규지만 저장 완료 후는 기존 사용자 취급
-////        Authentication auth = new CustomAuthenticationToken(result); // 커스텀 토큰 사용
-////        SecurityContextHolder.getContext().setAuthentication(auth);
-//        return  newUser;
-//    }
-
     @PostMapping("/signup")
-    public void signup(@AuthenticationPrincipal OAuthUserInfo oauthUser,
-                       @RequestBody Map<String, String> body,
+    public void signup(@RequestBody Map<String, String> body,
                        HttpServletRequest request,
                        HttpServletResponse response) throws IOException {
+
+        HttpSession session = request.getSession(false);
+        if (session == null || session.getAttribute("oauth2User") == null) {
+            throw new IllegalStateException("세션에 OAuth 정보가 없습니다.");
+        }
+
+        // 세션에서 임시 저장해둔 OAuth 정보 꺼내오기
+        OAuthUserInfo oauthUser = (OAuthUserInfo) session.getAttribute("oauth2User");
 
         User newUser = User.builder()
                 .username(oauthUser.getUsername())
@@ -60,6 +44,9 @@ public class SignupController {
                 .build();
         newUser = oAuthRepository.save(newUser);
 
+        session.setAttribute("user", newUser);
+
+        // 로그인 리다이렉트 처리
         String targetUrl = loginRedirectService.handleLogin(newUser, request);
         response.sendRedirect(targetUrl);
     }
