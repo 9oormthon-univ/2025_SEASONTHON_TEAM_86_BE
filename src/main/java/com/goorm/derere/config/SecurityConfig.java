19a0e40ceda60b98c3847300b310b2bcab358493
duddns6290 <duddns6290@naver.com>
2025-09-06 19:00:01 +0900

    feat: 카카오 로그인 수정

--- src/main/java/com/goorm/derere/config/SecurityConfig.java
+++ src/main/java/com/goorm/derere/config/SecurityConfig.java
@@ -25,7 +25,10 @@ public class SecurityConfig {
         http.csrf((csrf) -> csrf.disable())
         .headers(headers -> headers.frameOptions(frame -> frame.disable())) // h2-console 허용
         .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/**", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/auth/**").permitAll()
+                .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/swagger-ui/**",
+                        "/v3/api-docs/**",
+                        "/swagger-resources/**",
+                        "/webjars/**", "/**", "/auth/**").permitAll()
                 .anyRequest().authenticated() // 그 외 요청은 로그인 필요
         )
         .logout(logout -> logout
